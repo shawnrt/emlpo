b8a9c2c4406a844c12a5956078b0460a9f3b3c49
shawnb <shawn@cancoin.co>
2016-09-05 08:57:07 -0400

    Mailers

--- test/mailers/previews/new_membership_mailer_preview.rb
+++ test/mailers/previews/new_membership_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/new_membership_mailer
 class NewMembershipMailerPreview < ActionMailer::Preview
 
+  def welcome_email
+    NewMembershipMailer.welcome_email(User.first)
+  end
+
+  def welcome_email_admin
+    NewMembershipMailer.welcome_email_admin(User.first)
+  end
+
 end
